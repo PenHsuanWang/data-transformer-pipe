b7b5c923bb39e95e3bd2a66515b63ec94167801f
PenHsuanWang <willy118116@gmail.com>
2025-06-13 23:06:10 +0800

    Initialize project with CI, tests, and tooling

---
+++ tests/test_transformer.py
@@ -0,0 +1,6 @@
+from data_transformer_pipe.transformer import DataTransformer
+
+def test_transformer_returns_input():
+    transformer = DataTransformer()
+    data = {"key": "value"}
+    assert transformer.transform(data) == data
